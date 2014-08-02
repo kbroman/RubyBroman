b17abe764378cb934de18adaa9aa1aed5ef4a058
Karl Broman <kbroman@gmail.com>
2014-08-01 23:36:02 -0500

    json_reduce_digits: reduce the number of digits in a json file

---
+++ json_reduce_digits.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# reduce the number of digits in a json file
+
+ifilename = ARGV[0]
+ofilename = ifilename.sub(/(.+)\.json/, '\1_rev.json')
+
+print "#{ifilename} -> #{ofilename}\n"
+
+ofile = open(ofilename, 'w')
+open(ifilename).each do |line|
+    ofile.print line.gsub(/(\d+\.\d\d\d\d)\d+/, '\1')
+end
