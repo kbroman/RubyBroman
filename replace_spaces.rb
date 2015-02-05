11f25d0da1228f503c7df36ba2a66c5e771456ac
Karl Broman <kbroman@gmail.com>
2015-02-05 14:13:13 -0600

    Add replace_spaces.rb: replace spaces in a file name with underscores

---
+++ replace_spaces.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# replace spaces in a file name with underscores
+
+file = ARGV.join(' ')
+newfile = file.gsub(/\s+/, '_')
+puts "#{file} -> #{newfile}"
+`mv "#{file}" #{newfile}`
